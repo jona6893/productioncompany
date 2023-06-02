1f86e6e3eae36df75524517e951447ca7275942d
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-06-03 01:49:04 +0200

    slow but steady

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <div className="w-fit m-auto flex flex-col gap-4 items-center p-4">
+        <h2 className="text-3xl">Contact</h2>
+        <address className="flex flex-col gap-2 items-center">
+          Write to us at:
+          <a href="mailto:webmaster@example.com">webmaster@example.com</a>
+       
+          
+          <span>Call us on: <a href="tel:76482753">76 48 27 53</a></span>
+        
+          Københavngade 32A, <br /> københavn 2200
+          <br />
+          Danmark
+        </address>
+      </div>
+    </div>
+  );
 }
 
 export default Contact
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import { motion, AnimatePresence } from "framer-motion";
+import Head from "next/head";
 
 // Define your animations
 const pageVariants = {
@@ -17,56 +18,70 @@ const pageVariants = {
 const pageTransition = {
   type: "tween",
   ease: "easeInOut",
-  duration: 0.5,
+  duration: 0.3,
 };
 
 export default function Home() {
   const [curMenu, setCurMenu] = useState(0);
 
   return (
-    <main className="grid mainGrid mt-12 gap-4 p-4">
-      <div>
-        <Menu setCurMenu={setCurMenu} />
-      </div>
-      <AnimatePresence mode="wait">
-        {curMenu === 0 && (
-          <motion.div
-            key="gallery"
-            initial="initial"
-            animate="in"
-            exit="out"
-            variants={pageVariants}
-            transition={pageTransition}
-          >
-            <Gallery />
-          </motion.div>
-        )}
-        {curMenu === 1 && (
-          <motion.div
-            key="contact"
-            initial="initial"
-            animate="in"
-            exit="out"
-            variants={pageVariants}
-            transition={pageTransition}
-          >
-            <Contact />
-          </motion.div>
-        )}
-        {curMenu === 2 && (
-          <motion.div
-            key="about"
-            initial="initial"
-            animate="in"
-            exit="out"
-            variants={pageVariants}
-            transition={pageTransition}
-          >
-            <About />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </main>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <main className="grid mainGrid mt-12 gap-4 p-4">
+        <div>
+          <Menu setCurMenu={setCurMenu} />
+        </div>
+        <AnimatePresence mode="wait">
+          {curMenu === 0 && (
+            <motion.div
+              key="gallery"
+              initial="initial"
+              animate="in"
+              exit="out"
+              variants={pageVariants}
+              transition={pageTransition}
+            >
+              <Gallery />
+            </motion.div>
+          )}
+          {curMenu === 1 && (
+            <motion.div
+              key="contact"
+              initial="initial"
+              animate="in"
+              exit="out"
+              variants={pageVariants}
+              transition={pageTransition}
+            >
+              <Contact />
+            </motion.div>
+          )}
+          {curMenu === 2 && (
+            <motion.div
+              key="about"
+              initial="initial"
+              animate="in"
+              exit="out"
+              variants={pageVariants}
+              transition={pageTransition}
+            >
+              <About />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </main>
+    </>
   );
 }
 

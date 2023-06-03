2e7db4a6c4ce2e139304e12ca8c8c193bf3ac1fc
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-06-03 19:10:11 +0200

    nextjs image tag

--- src/app/components/Gallery.jsx
+++ src/app/components/Gallery.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -21,12 +22,13 @@ function Gallery({ isMobile }) {
 
   return (
     <>
-      
       <div className="grid md:grid-cols-3 gap-3 text-white pt-4">
         <div className="relative group col-span-2 overflow-hidden">
-          <img
+          <Image
             className={`block w-full h-full scale-[1.12]`}
-            src={urls[3]}
+            src={"/" + urls[3]}
+            width={1920}
+            height={1080}
             alt=""
           />
           <div className="bg-yellow-500/25 w-full h-full absolute top-0 opacity-0 group-hover:opacity-100 duration-300 flex items-center justify-center">
@@ -35,9 +37,13 @@ function Gallery({ isMobile }) {
         </div>
 
         <div className="relative group">
-          <img
-            className={`block w-full h-full object-cover object-right ${isMobile && "aspect-square"}`}
-            src={urls[7]}
+          <Image
+            className={`block w-full h-full object-cover object-right ${
+              isMobile && "aspect-square"
+            }`}
+            src={"/" + urls[7]}
+            width={1920}
+            height={1080}
             alt=""
           />
           <div className="bg-emerald-500/25 w-full h-full absolute top-0 opacity-0 group-hover:opacity-100 duration-300 flex items-center justify-center cursor-pointer">
@@ -45,24 +51,38 @@ function Gallery({ isMobile }) {
           </div>
         </div>
         <div className="relative group flex items-center justify-center">
-          <img className={`invert block w-full m-auto`} src={urls[1]} alt="" />
+          <Image
+            className={`invert block w-full m-auto`}
+            src={"/" + urls[1]}
+            width={1600}
+            height={1157}
+            alt=""
+          />
           <div className="bg-black/75 w-full h-full absolute top-0 opacity-0 group-hover:opacity-100 duration-300 flex items-center justify-center cursor-pointer">
             <h3 className="text-xl">AWARD & NOMINEES</h3>
           </div>
         </div>
 
         <div className="relative group col-span-2 overflow-hidden">
-          <img className={`block w-full scale-[1.33]`} src={urls[0]} alt="" />
+          <Image
+            className={`block w-full scale-[1.33]`}
+            src={"/" + urls[0]}
+            alt=""
+            width={1920}
+            height={1080}
+          />
           <div className="bg-rose-500/25 w-full h-full absolute top-0 opacity-0 group-hover:opacity-100 duration-300 flex items-center justify-center cursor-pointer">
             <h3 className="text-xl">HYBENHJERT</h3>
           </div>
         </div>
 
         <div className="relative group overflow-hidden">
-          <img
+          <Image
             className={`block w-full w-full h-full object-cover aspect-square scale-[1.2]`}
-            src={urls[5]}
+            src={"/" + urls[5]}
             alt=""
+            width={1920}
+            height={1080}
           />
           <div className="bg-yellow-500/25 w-full h-full absolute top-0 opacity-0 group-hover:opacity-100 duration-300 flex items-center justify-center cursor-pointer">
             <h3 className="text-xl">HOLD KÆFT</h3>
@@ -70,10 +90,12 @@ function Gallery({ isMobile }) {
         </div>
 
         <div className="relative group overflow-hidden">
-          <img
+          <Image
             className={`block w-full w-full h-full object-cover aspect-square object-left scale-[1.4]`}
-            src={urls[4]}
+            src={"/" + urls[4]}
             alt=""
+            width={1920}
+            height={1080}
           />
           <div className="bg-rose-500/25 w-full h-full absolute top-0 opacity-0 group-hover:opacity-100 duration-300 flex items-center justify-center cursor-pointer">
             <h3 className="text-xl">HYBENHJERT</h3>
@@ -81,17 +103,18 @@ function Gallery({ isMobile }) {
         </div>
 
         <div className={`relative group ${isMobile && "col-span-2"}`}>
-          <img
+          <Image
             className={`block w-full w-full h-full object-cover aspect-square object-right `}
-            src={urls[6]}
+            src={"/" + urls[6]}
             alt=""
+            width={1920}
+            height={1080}
           />
           <div className="bg-emerald-500/25 w-full h-full absolute top-0 opacity-0 group-hover:opacity-100 duration-300 flex items-center justify-center cursor-pointer">
             <h3 className="text-xl">BJARKE TØMMER SKRALD</h3>
           </div>
         </div>
       </div>
-
     </>
   );
 }

979cfd957c5d3c4be6ea37b070197deae568b4fc
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-06-08 23:22:39 +0200

    layout changes to

--- src/pages/films/[id].jsx
+++ src/pages/films/[id].jsx
@@ -9,26 +9,58 @@ function Film({ film }) {
   console.log(film);
 
   return (
-    <section className="text-white">
-      <h1 className="text-2xl">
-        {film.tItle}{" "}
-        <span className="bg-yellow-500 hover:bg-yellow-400 p-1 rounded text-base cursor-pointer">
-          <a className="text-black" href={"wwww.imdb.com"}>
-            IMDB
-          </a>
-        </span>
-      </h1>
-      <h4>{film.tagline}</h4>
-      <p>Release: {film.release}</p>
-      <p>Status: {film.stage}</p>
-      <p>Type: {film.type}</p>
-      <img className="w-60 " src={film.poster.url} alt="" />
-      {film.crewMembers.map((member) => (
-        <div>
-          <p>{member.function}</p>
-          <p>{member.name}</p>
+    <section className="text-white p-8 flex flex-col gap-16">
+      <div className="flex flex-col items-center gap-8">
+        <img className="w-96 " src={film.poster.url} alt="" />
+        <div className="flex flex-col gap-4">
+          <h1 className="text-5xl">
+            {film.tItle}{" "}
+            <span className="bg-yellow-500 hover:bg-yellow-400 p-1 rounded text-base cursor-pointer">
+              <a className="text-black" href={"wwww.imdb.com"}>
+                IMDB
+              </a>
+            </span>
+          </h1>
+          <h4>{film.tagline}</h4>
+          <p className="max-w-[48ch]">Description: {film.description.html}</p>
+          <p>
+            {" "}
+            <span className="text-sm text-gray-300">Release:</span>
+            {film.release}
+          </p>
+          <p>
+            <span className="text-sm text-gray-300">Status:</span> {film.stage}
+          </p>
+          <p>
+            <span className="text-sm text-gray-300">Type:</span> {film.type}
+          </p>
         </div>
-      ))}
+      </div>
+        <h3 className="text-center text-2xl">CREW</h3>
+      <div className="grid grid-cols-2">
+        <div className="grid gap-4 justify-end">
+          {film.crewMembers.map((member, i) => {
+              return (
+                <div className="bg-gray- mx-w-96 p-4 rounded text-end">
+                  <p >{member.function.toUpperCase()}:</p>
+               
+                </div>
+              );
+       
+          })}
+        </div>
+        <div className="grid gap-4 justify-start">
+          {film.crewMembers.map((member, i) => {
+            console.log(member.name)
+              return (
+                <div className="bg-gray- mx-w-96 p-4 rounded text-start">
+                  <p className="font-bold">{member.name}</p>
+                </div>
+              );
+        
+          })}
+        </div>
+      </div>
     </section>
   );
 }

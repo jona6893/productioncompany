dc1e6a76fe5ff083ee93bb12c078fcce174f3e71
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-06-11 22:38:34 +0200

    google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -16,20 +16,21 @@ export default function MyApp({ Component, pageProps }) {
           content="U.S.H Media, a leading film production company, expertly transforms unique narratives into captivating cinematic experiences. Discover why filmmakers worldwide trust us for exceptional production quality and creative storytelling. Be part of the journey where imagination meets the silver screen!"
         ></meta>
 
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-KWVK03Q1HR"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments)}
-              gtag('js', new Date());
-              gtag('config', 'G-KWVK03Q1HR');
-            `,
-          }}
-        ></script>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
       </Head>
       <Layout>
         <Component {...pageProps} />
